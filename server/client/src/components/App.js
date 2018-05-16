501ec2b724093bae883f5d924c6ba0c290621498
Dylan j <dj0759@gmail.com>
2018-05-15 21:11:44 -0700

    Reformatted navbar & implemented dynamic routing

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+
 import Header from './Header';
 import Dashboard from './Dashboard';
 
@@ -8,19 +12,28 @@ const SurveyNew = () =>  <h2>SurveyNew</h2>;
 
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Route path="/" component={Header}/>
-                    <Route exact path="/" component={Landing}/>
-                    <Route exact path="/dashboard" component={Dashboard}/>
-                    <Route exact path="/newSurvey" component={SurveyNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
-    )
+class App extends Component {
+    componentDidMount(){
+
+        this.props.fetchUser();
+
+    }
+    render(){
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing}/>
+                        <Route exact path="/dashboard" component={Dashboard}/>
+                        <Route exact path="/newSurvey" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App; 
+export default connect(null, actions)(App); 
+
+
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react';
-
+import { connect } from 'react-redux';
 
 class Header extends Component{
+    renderContent(){
+        switch(this.props.auth){
+            case null:
+                return (<li>
+                    <a href="/auth/google">Login With Google</a>
+                </li>            );
+            case false:
+                return (<li>
+                    <a href="/auth/google">Login With Google</a>
+                </li>            );
+            default:
+                return( <div><li>
+                <a href="/newSurvey">New Survey</a>
+            </li>
+            <li>
+                <a href="/dashboard">Dashboard</a>
+            </li>
+            <li>
+                <a href="/api/logout">Logout</a>
+            </li></div>);
+        }
+    }
     render(){
         return(
             <div>
@@ -9,15 +31,10 @@ class Header extends Component{
             <div class="nav-wrapper">
            <a className="left brand-logo" href="/">Emaily</a>
            <ul className="right">
-            <li>
-                <a href="/newSurvey">New Survey</a>
-            </li>
-            <li>
-                <a href="/dashboard">Dashboard</a>
-            </li>
-            <li>
-                <a href="/auth/google">Login With Google</a>
-            </li>
+           {this.renderContent()}
+
+
+
            </ul>
             </div>
           </nav>
@@ -26,5 +43,8 @@ class Header extends Component{
         )
     }
 }
+function mapStateToProps({auth}){
+    return{ auth: auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
