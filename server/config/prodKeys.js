0608dfcdc8d136b863ccc5441ff2cdcc5d6ab3e0
Dylan j <dj0759@gmail.com>
2018-05-16 19:03:42 -0700

    Keys

--- server/config/prodKeys.js
+++ server/config/prodKeys.js
@@ -1,9 +1,4 @@
-module.exports = {
-    googleClientID: '47389202637-vfe2jrtstem6a030on77djlfborvvq6k.apps.googleusercontent.com',
-    googleClientSecret: 'iRNwcQH5j9Y1E6L0T2RMggqi',
-    MongoURL: 'mongodb://user1:@#)IJ#J#F)J#@)IJ#@IFJ#K@JF)#I@JF)I#JF)I#J@I)#J@()#J@FIJFPO#JKj3pijfikj3f20@ds123790.mlab.com:23790/emaily-prod',
-    cookieKeys: ['a;lsjfdlajsldfjasjdf;2ef2f242423H#&@$*&@#H#klhjwdh23hd2h329(*(#*(@h9hd3982h23', 'l2kj32jf02jk23f2#&^*@HD#)(#Uj23d90j2f30ijf320ijf2ihjf0i2h30if@#()*#$I@P#I_D@#)Kh2302h3f0hi230ih23f0h23f0h', '023fj23kjf23jk23jokfj2okj#)@(#JD)(#@JDPOK#@)(#J@)(DJ#J@)#(J23kjf2kjfkhkshfiohd0ihfh*#HIH#Kh20h30h*#H08h3hkl']
-};
+
 
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
