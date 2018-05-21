8107d8b6f4e47c479ee8b5504eb272244a27a779
Dylan j <dj0759@gmail.com>
2018-05-20 18:20:53 -0700

    BuyCredits Styling

--- server/client/src/components/BuyCredits.js
+++ server/client/src/components/BuyCredits.js
@@ -7,26 +7,39 @@ class BuyCredits extends Component{
         return(
             <div>
                 <h1>Buy Credits</h1>
-                <Card className='small left'>
+                <div class="row">
+                <div class="col s3">
+                <Card>
                    <h4>5$</h4>
                    <h4>500 Credits</h4>
                     <Payments description="500 Credits for $5" amount={500} credits={500} />
                 </Card>
-                <Card className='small left'>
+                </div>
+                <div class="col s3">
+                <Card >
                     <h4>10$</h4>
                     <h4>1100 Credits</h4>
                     <Payments description="1100 Credits for $10" amount={1000} credits={1100} />
                 </Card>
-                <Card className='small left'>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col s3">
+                <Card>
                     <h4>15$</h4>
                     <h4>1600 Credits</h4>
                     <Payments description="1600 Credits for $15" amount={1500} credits={1600} />
                 </Card>
-                <Card className='small left'>
+                </div>
+                <div class="col s3">
+                <Card >
                     <h4>20$</h4>
                     <h4>2400 Credits</h4>
                     <Payments description="2400 Credits for $20" amount={2000} credits={2400} />
                 </Card>
+                </div>
+                
+                </div>
             </div>
         )
     }
